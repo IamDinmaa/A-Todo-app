8e2717a4a3389c8af64c5f47765077d1a301a6f2
IamDinmaa <chidinmafavournwakor@gmail.com>
2022-11-28 17:56:28 +0100

    initialise

--- src/components/NewTodo/TodoForm.js
+++ src/components/NewTodo/TodoForm.js
@@ -28,7 +28,7 @@ const TodoForm = (props) => {
     <form onSubmit={submitHandler}>
       <div className=".new-todo__controls">
         <div className="new-todo__control">
-          <label>What i want to do</label>
+          <label>Hey there! What do you want to do?</label>
           <input
             type="text"
             value={enteredTitle}
